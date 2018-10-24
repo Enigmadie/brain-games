6bf38d19e989f0450e0f569f9ce0cb77af4f7682
Enigmadie <gagen666@mail.ru>
2018-10-24 17:45:31 +0300

    fix index.js for 2 stage

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ console.log('Welcome to the Brain Games!');
 
 export default () => {
   const greetingUser = readlineSync.question('May I have your name? ');
-  return console.log(`Hello, ${greetingUser}!`);
+  console.log(`Hello, ${greetingUser}!`);
 };
