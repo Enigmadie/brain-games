2bff8b10bfdcb2b349e903a1d2f046e411194d2f
Enigmadie <gagen666@mail.ru>
2018-10-25 15:39:49 +0300

    fix stage 4 add new asciinema

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -10,46 +10,25 @@ export default () => {
   console.log(`Hello ${userName}!`);
   // function generation random integer
   const randomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-  // main iter
+  // main iter body
   const iter = (random, acc) => {
-    if (acc === 3) {
-      return console.log(`Congratulations, ${userName}`);
+    const resultForWin = 3;
+    const predicate = (random % 2 === 0);
+    if (acc === resultForWin) {
+      console.log(`Congratulations, ${userName}!`);
+      return;
     }
-    // even random value
-    if (random % 2 === 0) {
-      console.log(`Question ${random}`);
-      const answerEven = readlineSync.question('Your answer: ', {
-        trueValue: ['yes'],
-        falseValue: ['no'],
-      });
-      if (answerEven === true) {
-        console.log('Correct!');
-        return iter(randomInt(0, 99), acc + 1);
-      }
-      if (answerEven === false) {
-        console.log('"no" is wrong answer ;(. Correct answer was "yes"');
-        console.log(`Let's try again, ${userName}!`);
-      } else {
-        console.log('Incorrect answer ;(. Next time select "yes" or "no"');
-      }
+    const trueAnswer = (predicate) ? 'yes' : 'no';
+    console.log(`Question ${random}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === trueAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${trueAnswer}`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
-    // odd random value
-    if (random % 2 !== 0) {
-      console.log(`Question ${random}`);
-      const answerOdd = readlineSync.question('Your answer: ', {
-        trueValue: ['no'],
-        falseValue: ['yes'],
-      });
-      if (answerOdd === true) {
-        console.log('Correct!');
-        return iter(randomInt(0, 99), acc + 1);
-      } if (answerOdd === false) {
-        console.log('"yes" is wrong answer ;(. Correct answer was "no"');
-        console.log(`Let's try again, ${userName}!`);
-      } else {
-        console.log('Incorrect answer ;(. Next time select "yes" or "no"');
-      }
-    }
+    iter(randomInt(0, 99), acc + 1);
   };
-  return iter(randomInt(0, 99), 0);
+  iter(randomInt(0, 99), 0);
 };
